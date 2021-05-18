261092e735701c375fbd1e0725ed8ba33a3d1d62
Sanket Bhiogade <sanket.bhiogade95@gmail.com>
2021-05-18 20:57:53 +0530

    Checkpoint 13

--- scripts/common.js
+++ scripts/common.js
@@ -129,3 +129,33 @@ let footerTemplate = `<footer class="header-footer">
 </footer>`;
 
 document.getElementById("footers").innerHTML = footerTemplate;
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector(
+          "body").style.visibility = "hidden";
+        document.querySelector(
+          "#loader").style.visibility = "visible";
+    } else {
+        document.querySelector(
+          "#loader").style.display = "none";
+        document.querySelector(
+          "body").style.visibility = "visible";
+    }
+}
+
+var xhr = new XMLHttpRequest();
+
+function searchbook() {
+    var booksearch = document.getElementById('booksearch').value;
+    var getString = 'https://www.googleapis.com/books/v1/volumes?q=' + booksearch;
+    xhr.open('GET', getString);
+    xhr.send();   
+    xhr.onreadystatechange = function (data){
+       if(xhr.readyState === 4 && xhr.status === 200) {
+            var data = JSON.parse(xhr.response);
+           for (i=0; i<data.items.length; i++){
+                document.getElementById('displaysearchedresults').innerHTML += "<p>" + data.items[i].volumeInfo.title + "<p>"
+           }    
+       }
+   }
+}
